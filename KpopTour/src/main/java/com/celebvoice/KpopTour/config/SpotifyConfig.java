60bc28c2e17ae52d0ffb4a09a8912a1b4f870503
vinyeee <loveyebin077@naver.com>
2023-07-31 22:35:14 +0900

    new front

--- KpopTour/src/main/java/com/celebvoice/KpopTour/config/SpotifyConfig.java
+++ KpopTour/src/main/java/com/celebvoice/KpopTour/config/SpotifyConfig.java
@@ -24,7 +24,6 @@ public class SpotifyConfig {
     }
 
 
-
     public static String accessToken(SpotifyApi spotifyApi) {
         ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
         try {
